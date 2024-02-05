f8c46e4e24b89e27109c30980f1481989466b505
Viacheslav Korbut <minowaman777@gmail.com>
2024-02-05 20:10:43 +0200

    Refactored some things

--- tm_web/src/main/java/com/taskmanager/controller/UpdateTaskServlet.java
+++ tm_web/src/main/java/com/taskmanager/controller/UpdateTaskServlet.java
@@ -26,27 +26,25 @@ public class UpdateTaskServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            Integer.parseInt(request.getParameter("id"));
-        } catch (NumberFormatException e) {
-            request.setAttribute("message", request.getParameter("id"));
-            request.setAttribute("url", "/edit-task");
-            request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request, response);
-        }
-        int id = Integer.parseInt(request.getParameter("id"));
-        if (taskRepository.read(id) == null) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            request.setAttribute("message", id);
-            request.setAttribute("url", "/edit-task");
-            request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request, response);
-        } else {
+            int id = Integer.parseInt(request.getParameter("id"));
             task = taskRepository.read(id);
-            request.setAttribute("task", task);
-            request.getRequestDispatcher("/WEB-INF/pages/edit-task.jsp").forward(request, response);
+            if (task == null) {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                request.setAttribute("message", "Task with ID " + id + " not found!");
+                request.setAttribute("url", "/edit-task");
+                request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request, response);
+            } else {
+                request.setAttribute("task", task);
+                request.getRequestDispatcher("/WEB-INF/pages/edit-task.jsp").forward(request, response);
+            }
+        } catch (NumberFormatException e) {
+            request.setAttribute("message", "You must enter the task id!");
+            request.setAttribute("url", "/edit-task");
+            request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request, response);
         }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         task.setTitle(request.getParameter("title"));
         task.setPriority(Priority.valueOf(request.getParameter("priority")));
         taskRepository.update(task, task.getId());
