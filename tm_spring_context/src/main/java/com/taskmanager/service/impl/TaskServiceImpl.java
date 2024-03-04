a9acd86f4ec3360a02224dd4e33aa69e8e7573bb
Viacheslav Korbut <minowaman777@gmail.com>
2024-03-04 19:12:12 +0200

    Completed TaskServiceImpl

--- tm_spring_context/src/main/java/com/taskmanager/service/impl/TaskServiceImpl.java
+++ tm_spring_context/src/main/java/com/taskmanager/service/impl/TaskServiceImpl.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -16,40 +17,79 @@ import java.util.List;
 public class TaskServiceImpl implements TaskService {
     private final ToDoService toDoService;
 
-    // TODO: implement method
     @Override
     public Task addTask(Task task, ToDo todo) {
-        return null;
+        if (task == null) {
+            throw new IllegalArgumentException("Task can`t be null");
+        }
+        var checkToDo = toDoService
+                .getAll()
+                .stream()
+                .filter(toDo -> toDo.equals(todo))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("ToDo not found"));
+        checkToDo.getTaskList().add(task);
+        return task;
     }
 
     @Override
     public Task updateTask(Task task) {
-        return null;
+        return getAll().stream()
+                .filter(t -> t.equals(task))
+                .findFirst()
+                .map(t -> {
+                    t.setName(task.getName());
+                    t.setPriority(task.getPriority());
+                    return t;
+                })
+                .orElseThrow(() -> new IllegalArgumentException("Task not found"));
     }
 
     @Override
     public void deleteTask(Task task) {
-
+        if (!getAll().contains(task)) {
+            throw new IllegalArgumentException("Task not found");
+        } else {
+            getAll().remove(task);
+        }
     }
 
     @Override
     public List<Task> getAll() {
-        return null;
+        return toDoService.getAll()
+                .stream()
+                .flatMap(toDo -> toDo.getTaskList().stream())
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Task> getByToDo(ToDo todo) {
-        return null;
+        if (todo == null) {
+            throw new IllegalArgumentException("ToDo can`t be null");
+        }
+        var checkTodo = toDoService.getAll()
+                .stream()
+                .filter(toDo -> toDo.equals(todo))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("ToDo not found"));
+        return checkTodo.getTaskList();
     }
 
     @Override
     public Task getByToDoName(ToDo todo, String name) {
-        return null;
+        return getByToDo(todo).stream()
+                .filter(task -> task.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Task not found"));
+
     }
 
     @Override
     public Task getByUserName(User user, String name) {
-        return null;
+        return toDoService.getByUser(user).stream()
+                .flatMap(toDo -> toDo.getTaskList().stream())
+                .filter(task -> task.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Task not found"));
     }
 
 }
